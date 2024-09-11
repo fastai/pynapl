f0f347f393c9fc55240d9442b9a8b85add35defd
Jeremy Howard <github@jhoward.fastmail.fm>
2024-09-12 05:58:25 +1000

    bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="pynapl-dyalog",
-    version='0.1.0post1',
+    version='0.1.0post2',
     description='Python - Dyalog APL interface',
     long_description="This package allows communication between Python and Dyalog APL.",
     url='https://github.com/fastai/pynapl',
